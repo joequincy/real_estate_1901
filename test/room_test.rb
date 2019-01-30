c32c6fbdca6af50f8b143d1815a0296ea7ad95cb
Jon Peterson <jon@pnwebs.com>
2019-01-30 09:13:44 -0700

    Complete Iteration 1

---
+++ test/room_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/room'
+
+class RoomTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_room_exists
+    assert_instance_of Room, @room
+  end
+
+  def test_room_attributes
+    assert_equal :bedroom, @room.category
+    assert_equal 130, @room.area
+  end
+end
